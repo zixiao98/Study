a0b401636391c425fcb03bfde297e27d266c62f0
zixiao <2568624492@qq.com>
2023-02-22 23:21:31 +0800

    数组拍平.js

---
+++ flat数组拍平.js
@@ -0,0 +1,32 @@
+let arr = [1, 2, [2, 4, [2, 1, [2],
+            [1, 4, 5, 6]
+        ],
+        [17, 28]
+    ],
+    [12, 32]
+];
+
+//自带方法 Array.prototype.flat(层数/infinity)
+console.log(arr.flat(Infinity))
+
+//手写版
+function _flat(arr) {
+    return arr.reduce((per, cur) => {
+        return per.concat(Array.isArray(cur) ? _flat(cur) : cur)
+    }, [])
+}
+console.log(_flat(arr))
+
+//手写版 - 参加拍平层数
+function _flat2(arr, deep) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+       if(Array.isArray(arr[i]) && deep > 0){
+          res =  res.concat(_flat2(arr[i],deep - 1))//这里记得给res重新赋值，因为concat返回的是新数组，不会改变原数组
+       }else{
+        res.push(arr[i])
+       } 
+    }
+    return res
+}
+console.log(_flat2(arr,5))
