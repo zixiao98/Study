9455ac511b7125e309dd4e70b8d24a558d175fab
zixiao <2568624492@qq.com>
2023-02-24 23:11:18 +0800

    修复函数柯里化中，目标函数this可能丢失的问题

--- 函数柯里化.js
+++ 函数柯里化.js
@@ -7,15 +7,31 @@
 
 
 function add(x, y, z, j) {
+    console.log(this.name)
     return x + y + z + j
 }
 
 function curry(fn, ...args) {
     //边界判断
-    if(typeof fn !== 'function') throw new TypeError('the first is not a function');
+    if (typeof fn !== 'function') throw new TypeError('the first is not a function');
     //返回一个函数，后面将多次返回该函数
-    return function curried(...other) {
-        //判断接收到的参数是否符合形参个数要求
+    // return function curried(...other) {
+    //     //判断接收到的参数是否符合形参个数要求
+    //     let alen = [...args,...other];
+    //     if(alen.length < fn.length){
+    //         //继续收集参数
+    //         return (...others)=>{
+    //             return curried(...alen,...others)
+    //         }
+    //     }else {
+    //         //最后得到结果
+    //         return fn(...alen)
+    //     }
+    // }
+    //上述写法会造成this丢失
+    //改为
+    let curried = (...other) => {
+        // 判断接收到的参数是否符合形参个数要求
         let alen = [...args,...other];
         if(alen.length < fn.length){
             //继续收集参数
@@ -24,9 +40,10 @@ function curry(fn, ...args) {
             }
         }else {
             //最后得到结果
-            return fn(...alen)
+            return fn.call(this,...alen)//此时箭头函数内部的“this”会绑定curry环境中的this
         }
     }
-
+    return curried
 }
-console.log(curry(add,111)(12,31,42))
+console.log(curry(add, 111)(12, 31, 42))
+console.log(curry.call({name:'lzj'},add, 111)(12, 31, 42))
